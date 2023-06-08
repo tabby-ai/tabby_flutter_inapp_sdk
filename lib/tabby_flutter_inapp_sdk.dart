f37b0015a22252a629bed11f01622ccd5213c550
ilya.kuznetsov <kromsik@icloud.com>
2023-06-08 14:38:49 +0400

    👌 IMPROVE: export TabbyPresentationSnippetNonStantard

--- lib/tabby_flutter_inapp_sdk.dart
+++ lib/tabby_flutter_inapp_sdk.dart
@@ -2,6 +2,7 @@ library tabby_flutter_inapp_sdk;
 
 export './src/internal/tabby_checkout_snippet.dart';
 export './src/internal/tabby_presentation_snippet.dart';
+export './src/internal/tabby_presentation_snippet_non_standard.dart';
 export './src/internal/tabby_sdk.dart';
 export './src/internal/tabby_web_view.dart';
 export './src/internal/utils.dart';
